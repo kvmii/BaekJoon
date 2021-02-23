8acb10fd368c1fa3f9c5c0e0768b9f095d69e997
Jung Min Kim <50435968+kvmii@users.noreply.github.com>
2021-02-23 15:01:28 +0900

    Update gt_2206.cpp

--- graph traversal/gt_2206.cpp
+++ graph traversal/gt_2206.cpp
@@ -1,74 +1,64 @@
 #include <iostream>
 #include <queue>
 #include <tuple>
-#define MAX 1000
+#define MAX 1001
 using namespace std;
 
 int X[] = { 1, -1, 0, 0 };
 int Y[] = { 0, 0, -1, 1 };
 char map[MAX][MAX];
-int visited[MAX][MAX];
+int visited[MAX][MAX][2];
 int n, m;
-int INT = 2000;
-int result = INT;
-queue<tuple<int, int, int>> q;
 
 
-void maze() {
-	q.push(make_tuple(0, 0, 1));
+int maze() {
+	queue<tuple<int, int, int>> q;
+	q.push(make_tuple(0, 0, 0));
+	visited[0][0][0] = 1;
 
 	while (!q.empty()) {
 		int x = get<0>(q.front());
 		int y = get<1>(q.front());
 		int cnt = get<2>(q.front());
 
-		if (x == n - 1 && y == m - 1)
-			break;
-
 		q.pop();
+
+		if (x == n - 1 && y == m - 1)
+			return visited[x][y][cnt];
 
 		for (int k = 0; k < 4; k++) {
 			int dx = x + X[k];
 			int dy = y + Y[k];
 
 			if (dx >= 0 && dx < n && dy >= 0 && dy < m) {
+				if (visited[dx][dy][cnt])
+					continue;
 
-				if (map[dx][dy] == '0' && visited[dx][dy] != 1) {
-					visited[dx][dy] = 1;
+				if (map[dx][dy] == '0') {
+					// 벽이 없다면
+					visited[dx][dy][cnt] = visited[x][y][cnt] + 1;
+					q.push(make_tuple(dx, dy, cnt));
+				}
+
+				if (map[dx][dy] == '1' && cnt == 0) {
+					// 벽이 있고 아직 부시지 않았다면
+					visited[dx][dy][cnt + 1] = visited[x][y][cnt] + 1;
+					// 벽을 하나 부수기
 					q.push(make_tuple(dx, dy, cnt + 1));
 				}
 			}
 		}
 	}
-	int a = get<0>(q.front());
-	int b = get<1>(q.front());
-	int c = get<2>(q.front());
-	if (a == n - 1 && b == m - 1)
-		result = min(c, result);
+
+	return -1;
 }
 
-void clear() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			visited[i][j] = 0;
-		}
-	}
-}
-
-void wall() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (map[i][j] == '1') {
-				map[i][j] = '0';
-				clear();
-				maze();
-				map[i][j] = '1';
-			}
-		}
-	}
-}
 
 int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
@@ -76,11 +66,7 @@ int main() {
 		}
 	} 
 
-	wall();
-
-	if (result == INT)
-		cout << "-1" << endl;
-	else
-		cout << result << endl;
-	return 0;
+	cout << maze();
+	// 처음에 연구소 문제와 비슷하게 풀었으나 시간초과가 발생
+	// bfs함수 안에서 해결
 }
